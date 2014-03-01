0fd36d9c2e297ba2ccaa9fb53645f1b535b3c987
tankwanghow <tankwanghow@gmail.com>
2014-03-01 11:01:59 +0800

    added diet_glpsol lib

---
+++ lib/diet_glpsol.rb
@@ -0,0 +1,133 @@
+class DietGlpsol
+  def self.solution_for_formula formula, filename, solver='glpsol --math'
+    gmpl_for_formula formula, filename
+    solution_for_gmpl filename, solver
+  end
+
+  def self.solution_for_gmpl filename, solver
+    a = `#{solver + " " + filename}`
+    `rm #{filename}`
+    formula = a.scan(/FORMULA_START(.+?)FORMULA_END/)
+    specs = a.scan(/SPECS_START(.+?)SPECS_END/)
+    if formula.empty? or specs.empty?
+      nil
+    else
+      { formula: formula.flatten[0].split('|').compact, specs: specs.flatten[0].split('|').compact }
+    end
+  end
+
+  def self.gmpl_for_formula formula, filename
+    f = File.new filename, 'w'
+    f.puts varibles formula.ingredients
+    f.puts objective_function formula.ingredients
+    f.puts nutrient_expressions_constraints formula.nutrients, formula.ingredients
+    f.puts constraint_100 formula.ingredients
+    f.puts ingredients_constraints formula.ingredients
+    f.puts 'solve;'
+    f.puts "printf 'FORMULA_START';\n"
+    f.puts printf_statement_for_ingredients formula.ingredients
+    f.puts "printf 'FORMULA_END';\n"
+    f.puts "printf 'SPECS_START';\n"
+    f.puts printf_statement_for_nutrients formula.nutrients.map { |n| n.nutrient }
+    f.puts "printf 'SPECS_END';\n"
+    f.puts 'end;'
+    f.close
+  end
+
+  def self.constraint_100 ingredients
+    "PERC: " + ingredients.map { |i| "+p_#{i.id}"}.join(" ") + " = 1;\n"
+  end
+
+  def self.varible ingredient
+    "var p_#{ingredient.id} >= 0;"
+  end
+
+  def self.varibles ingredients
+    ingredients.map { |i| varible(i) }.join("\n") + "\n"
+  end
+
+  def self.ingredient_expression ingredient
+    "+#{ingredient.cost}*p_#{ingredient.id}"
+  end
+
+  def self.objective_function ingredients
+    "minimize cost: " + ingredients.map { |i| ingredient_expression(i) }.join(" ") + ";\n"
+  end
+
+  def self.ingredient_constraint ingredient
+    constraint = constraint(ingredient, "p_#{ingredient.id}")
+    constraint ? "s.t.pc_#{ingredient.id}: " + constraint(ingredient, "p_#{ingredient.id}") : nil
+  end
+
+  def self.ingredients_constraints ingredients
+    ingredients.map { |i| ingredient_constraint(i) }.compact.join("\n") + "\n"
+  end
+
+  def self.nutrient_expression nutrient, ingredient
+    innu= ingredient.nutrients.find { |t| t.nutrient.id == nutrient.id }
+    innu.value > 0 ? "+#{innu.value}*p_#{ingredient.id}" : nil
+  end
+
+  def self.nutrient_expressions nutrient, ingredients
+    ingredients.map { |i| nutrient_expression(nutrient, i) }.compact.join(" ")
+  end
+
+  def self.nutrient_expressions_constraint nutrient_spec, ingredients
+    exp = constraint(nutrient_spec, nutrient_expressions(nutrient_spec.nutrient, ingredients))
+    if exp != " >= 0;"
+      "n_#{nutrient_spec.nutrient.id}: " + constraint(nutrient_spec, nutrient_expressions(nutrient_spec.nutrient, ingredients))
+    else
+      "n_#{nutrient_spec.nutrient.id}: " + ingredients.map { |i| "+p_#{i.id}"}.join(" ") + " >= 0;"
+    end
+  end
+
+  def self.nutrient_expressions_constraints nutrient_specs, ingredients
+    nutrient_specs.map { |n| nutrient_expressions_constraint(n, ingredients) }.compact.join("\n") + "\n"
+  end
+
+  def self.printf_statement_for_ingredient ingredient
+    "printf 'p_#{ingredient.id},%.6f,%.6f|', p_#{ingredient.id}.val, p_#{ingredient.id}.dual;"
+  end
+
+  def self.printf_statement_for_ingredients ingredients
+    ingredients.map { |i| printf_statement_for_ingredient(i) }.join("\n") + "\n"
+  end
+
+  def self.printf_statement_for_nutrient nutrient
+    "printf 'n_#{nutrient.id},%.6f|', n_#{nutrient.id}.val;"
+  end
+
+  def self.printf_statement_for_nutrients nutrients
+    nutrients.map { |n| printf_statement_for_nutrient(n) }.join("\n") + "\n"
+  end
+
+private
+
+  def self.constraint object, expression=""
+    if object.max and object.min
+      gt_lt_constraint(object, expression)
+    elsif object.max and !object.min
+      lt_constraint(object, expression)
+    elsif object.min and !object.max
+      gt_constraint(object, expression)
+    else
+      gt_zero_constraint(object, expression)
+    end
+  end
+
+  def self.gt_lt_constraint object, expression
+    "#{object.min} <= #{expression} <= #{object.max};"
+  end
+
+  def self.gt_constraint object, expression
+    "#{expression} >= #{object.min};"
+  end
+
+  def self.lt_constraint object, expression
+    "#{expression} <= #{object.max};"
+  end
+
+  def self.gt_zero_constraint object, expression
+    "#{expression} >= 0;"
+  end
+end
---
+++ spec/lib/diet_glpsol_spec.rb
@@ -0,0 +1,202 @@
+require_relative '../../lib/diet_glpsol'
+
+describe DietGlpsol do
+  before(:each) do
+    @me      = double(id: 1, name: 'ME',      unit: 'kcal/kg')
+    @protein = double(id: 2, name: 'Protein', unit: '%')
+    @calcium = double(id: 3, name: 'Calcium', unit: '%')
+    @nothing = double(id: 4, name: 'Nothing', unit: 'n')
+    @formula_nutrients = [
+      double(nutrient: @me,      max: nil, min: 2800), 
+      double(nutrient: @protein, max:  16, min:  nil), 
+      double(nutrient: @calcium, max: 4.1, min:  3.8),
+      double(nutrient: @nothing, max: nil, min: nil)
+    ]
+    @formula1_nutrients = [
+      double(nutrient: @me,      max: nil, min: 8000), 
+      double(nutrient: @protein, max:  50, min:  nil), 
+      double(nutrient: @calcium, max: 4.1, min:  3.8),
+      double(nutrient: @nothing, max: nil, min: nil)
+    ]
+    @corn_nutrients = [
+      double(nutrient: @me,      value: 3350), 
+      double(nutrient: @protein, value:  8.3), 
+      double(nutrient: @calcium, value: 0.02),
+      double(nutrient: @nothing, value:    1)
+    ]
+    @soyabean_nutrients = [
+      double(nutrient: @me,      value: 2450), 
+      double(nutrient: @protein, value: 47.5), 
+      double(nutrient: @calcium, value: 0.30),
+      double(nutrient: @nothing, value:    2)
+    ]
+    @wheatbran_nutrients = [
+      double(nutrient: @me,      value: 1300), 
+      double(nutrient: @protein, value: 15.0), 
+      double(nutrient: @calcium, value: 0.10),
+      double(nutrient: @nothing, value:    4)
+    ]
+    @limestone_nutrients = [
+      double(nutrient: @me,      value:    0), 
+      double(nutrient: @protein, value:    0), 
+      double(nutrient: @calcium, value: 38.0),
+      double(nutrient: @nothing, value:    5)
+    ]
+    @mdcp_nutrients = [
+      double(nutrient: @me,      value:    0), 
+      double(nutrient: @protein, value:    0), 
+      double(nutrient: @calcium, value: 18.0),
+      double(nutrient: @nothing, value:    9)
+    ]
+    @corn      = double(id: 1, name: 'Corn',          cost: 0.9500, max: 0.70, min: 0.40, nutrients: @corn_nutrients)
+    @soyabean  = double(id: 2, name: 'Soyabean Meal', cost: 1.8000, max:  nil, min: 0.15, nutrients: @soyabean_nutrients)
+    @wheatbran = double(id: 3, name: 'Wheat Bran',    cost: 0.7900, max: 0.50, min: nil,  nutrients: @wheatbran_nutrients)
+    @limestone = double(id: 4, name: 'Limestone',     cost: 0.0650, max:  nil, min: nil,  nutrients: @limestone_nutrients)
+    @mdcp      = double(id: 5, name: 'MDCP',          cost: 1.7500, max:  nil, min: nil,  nutrients: @mdcp_nutrients)
+    @ingredients = [@corn, @soyabean, @wheatbran, @limestone, @mdcp]
+    @formula   = double(:ingredients => @ingredients, nutrients: @formula_nutrients)
+    @formula1   = double(:ingredients => @ingredients, nutrients: @formula1_nutrients)
+  end
+
+  it "should find the solution for formula" do
+    filename = "abc#{rand(5000)}xyz.mod"
+    DietGlpsol.solution_for_formula(@formula, filename, 'glpsol --math').should == {
+      formula: ["p_1,0.700000,0.000000", "p_2,0.167552,0.000000", "p_3,0.034230,0.000000", "p_4,0.098219,0.000000", "p_5,0.000000,1.903611"],
+      specs:   ["n_1,2800.000000", "n_2,14.282146", "n_3,3.800000", "n_4,1.663116"]
+    }
+  end
+
+  it "should delete the solved file" do
+    filename = "abc#{rand(5000)}xyz.mod"
+    DietGlpsol.gmpl_for_formula(@formula, filename)
+    DietGlpsol.solution_for_gmpl(filename, 'glpsol --math')
+    Proc.new { File.open(filename) }.should raise_error
+  end
+
+  it "should return nil, if solution not found" do
+    filename = "abc#{rand(5000)}xyz.mod"
+    DietGlpsol.gmpl_for_formula(@formula1, filename)
+    DietGlpsol.solution_for_gmpl(filename, 'glpsol --math').should == nil
+  end
+
+  it "should be return the solution, if soultion found" do
+    filename = "abc#{rand(5000)}xyz.mod"
+    DietGlpsol.gmpl_for_formula(@formula, filename)
+    DietGlpsol.solution_for_gmpl(filename, 'glpsol --math').should == {
+      formula: ["p_1,0.700000,0.000000", "p_2,0.167552,0.000000", "p_3,0.034230,0.000000", "p_4,0.098219,0.000000", "p_5,0.000000,1.903611"],
+      specs:   ["n_1,2800.000000", "n_2,14.282146", "n_3,3.800000", "n_4,1.663116"] }
+  end
+
+  it "should generate full gmpl file" do
+    filename = "abc#{rand(5000)}xyz.mod"
+    DietGlpsol.gmpl_for_formula(@formula, filename)
+    Proc.new { File.open(filename) }.should_not raise_error
+    `rm #{filename}`
+  end
+
+  it "should generate printf for nutrients" do
+    DietGlpsol.printf_statement_for_nutrients([@me, @protein, @calcium, @nothing]).should ==
+    "printf 'n_1,%.6f|', n_1.val;\n" +
+    "printf 'n_2,%.6f|', n_2.val;\n" +
+    "printf 'n_3,%.6f|', n_3.val;\n" +
+    "printf 'n_4,%.6f|', n_4.val;\n"
+  end
+
+  it "should generate printf for nutrient" do
+    DietGlpsol.printf_statement_for_nutrient(@me).should == "printf 'n_1,%.6f|', n_1.val;"
+  end
+
+  it "should generate printf for ingredients" do
+    DietGlpsol.printf_statement_for_ingredients(@ingredients).should == 
+    "printf 'p_1,%.6f,%.6f|', p_1.val, p_1.dual;\n" +
+    "printf 'p_2,%.6f,%.6f|', p_2.val, p_2.dual;\n" +
+    "printf 'p_3,%.6f,%.6f|', p_3.val, p_3.dual;\n" +
+    "printf 'p_4,%.6f,%.6f|', p_4.val, p_4.dual;\n" +
+    "printf 'p_5,%.6f,%.6f|', p_5.val, p_5.dual;\n"
+  end
+
+  it "should generate printf for ingredient" do
+    DietGlpsol.printf_statement_for_ingredient(@corn).should == "printf 'p_1,%.6f,%.6f|', p_1.val, p_1.dual;"
+  end
+
+  it "should generate nutrients expressions constraints" do
+    DietGlpsol.nutrient_expressions_constraints(@formula_nutrients, @ingredients).should == 
+    "n_1: +3350*p_1 +2450*p_2 +1300*p_3 >= 2800;\n" +
+    "n_2: +8.3*p_1 +47.5*p_2 +15.0*p_3 <= 16;\n" +
+    "n_3: 3.8 <= +0.02*p_1 +0.3*p_2 +0.1*p_3 +38.0*p_4 +18.0*p_5 <= 4.1;\n" +
+    "n_4: +1*p_1 +2*p_2 +4*p_3 +5*p_4 +9*p_5 >= 0;\n"
+  end
+
+  it "should generate gt zero constraint" do
+    DietGlpsol.nutrient_expressions(@nothing, @ingredients).should == "+1*p_1 +2*p_2 +4*p_3 +5*p_4 +9*p_5"
+  end
+
+  it "should generate nutrient l.t. expressions constraint" do
+    DietGlpsol.nutrient_expressions_constraint(@formula_nutrients[1], @ingredients).should == "n_2: +8.3*p_1 +47.5*p_2 +15.0*p_3 <= 16;"
+  end
+
+  it "should generate nutrient g.t. expressions constraint" do
+    DietGlpsol.nutrient_expressions_constraint(@formula_nutrients[0], @ingredients).should == "n_1: +3350*p_1 +2450*p_2 +1300*p_3 >= 2800;"
+  end
+
+  it "should generate nutrient g.t. and l.t. expressions constraint" do
+    DietGlpsol.nutrient_expressions_constraint(@formula_nutrients[2], @ingredients).should == "n_3: 3.8 <= +0.02*p_1 +0.3*p_2 +0.1*p_3 +38.0*p_4 +18.0*p_5 <= 4.1;"
+  end
+
+  it "should generate nutrient expressions" do
+    DietGlpsol.nutrient_expressions(@protein, @ingredients).should == "+8.3*p_1 +47.5*p_2 +15.0*p_3"
+  end
+
+  it "should not generate nutrient expression" do
+    DietGlpsol.nutrient_expression(@protein, @limestone).should == nil
+  end
+
+  it "should generate nutrient expression" do
+    DietGlpsol.nutrient_expression(@calcium, @corn).should == "+0.02*p_1"
+  end
+
+  it "should generate 100% default constraint" do
+    DietGlpsol.constraint_100(@ingredients).should == "PERC: +p_1 +p_2 +p_3 +p_4 +p_5 = 1;\n"
+  end
+
+  it "should generate ingredients constraints" do
+    DietGlpsol.ingredients_constraints(@ingredients).should ==
+    "s.t.pc_1: 0.4 <= p_1 <= 0.7;\n" +
+    "s.t.pc_2: p_2 >= 0.15;\n" +
+    "s.t.pc_3: p_3 <= 0.5;\n" +
+    "s.t.pc_4: p_4 >= 0;\n" +
+    "s.t.pc_5: p_5 >= 0;\n"
+  end
+
+  it "should generate zero constraint" do
+    DietGlpsol.ingredient_constraint(@limestone).should == "s.t.pc_4: p_4 >= 0;"
+  end
+
+  it "should generate l.t. ingredient constraint" do
+    DietGlpsol.ingredient_constraint(@wheatbran).should == "s.t.pc_3: p_3 <= 0.5;"
+  end
+
+  it "should generate g.t. ingredient constraint" do
+    DietGlpsol.ingredient_constraint(@soyabean).should == "s.t.pc_2: p_2 >= 0.15;"
+  end
+
+  it "should generate g.t. and l.t. ingredient constraint" do
+    DietGlpsol.ingredient_constraint(@corn).should == "s.t.pc_1: 0.4 <= p_1 <= 0.7;"
+  end
+
+  it "should generate minimize objective function" do
+    DietGlpsol.objective_function(@ingredients).should == "minimize cost: +0.95*p_1 +1.8*p_2 +0.79*p_3 +0.065*p_4 +1.75*p_5;\n"
+  end
+
+  it "should generate ingredient cost expression" do
+    DietGlpsol.ingredient_expression(@wheatbran).should == "+0.79*p_3"
+  end
+
+  it "should generate varible string for given ingredient" do
+    DietGlpsol.varible(@mdcp).should == "var p_5 >= 0;"
+  end
+
+  it "should generate_varibles string for given ingredients" do
+    DietGlpsol.varibles(@ingredients).should == "var p_1 >= 0;\nvar p_2 >= 0;\nvar p_3 >= 0;\nvar p_4 >= 0;\nvar p_5 >= 0;\n"
+  end
+end
