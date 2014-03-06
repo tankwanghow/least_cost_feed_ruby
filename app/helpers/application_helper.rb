b8f87028555d4ff3fbf738f8ecad430e2c016715
tankwanghow <tankwanghow@gmail.com>
2014-03-06 17:54:20 +0800

    display flash helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def render_flash
+    a = ''
+    top = 5
+    flash.each do |name, msg|
+      a << content_tag(:div, class: "alert alert-dismissable alert-#{name} col-md-offset-2 col-md-6", style: "top: #{top}px;") do
+        content_tag(:button, '&times;'.html_safe, class: 'close', data: { dismiss: 'alert' }) +  
+        content_tag(:div, content_tag(:strong, msg), id: "flash_#{name}", class: 'center-text') if msg.is_a?(String)
+      end
+      top = top + 55
+    end
+    flash.clear
+    a.html_safe
+  end
+
 end
