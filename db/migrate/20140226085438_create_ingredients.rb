29dbf5a004fc2775df1ede5bbd9cdb7eff0f5139
tankwanghow <tankwanghow@gmail.com>
2014-03-12 17:35:52 +0800

    added more fields

--- db/migrate/20140226085438_create_ingredients.rb
+++ db/migrate/20140226085438_create_ingredients.rb
@@ -2,11 +2,14 @@ class CreateIngredients < ActiveRecord::Migration
   def change
     create_table :ingredients do |t|
       t.belongs_to :user, null: false
-      t.string     :name,  null: false
-      t.decimal    :cost,  null: false, default: 0, precision: 12, scale: 4
+      t.string     :name, null: false
+      t.decimal    :cost, null: false, default: 0, precision: 12, scale: 4
+      t.string     :batch_no
+      t.text       :note
+      t.string     :status, null: false, default: 'pending'
       t.integer    :lock_version, null: false, default: 0
       t.timestamps
     end
-    add_index :ingredients, [:user_id, :name], unique: true
+    add_index :ingredients, [:user_id, :name, :batch_no], unique: true
   end
 end
