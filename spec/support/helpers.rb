ab9f0dedfc7eb3b0f2d2a87b3d3b47f25c36a450
tankwanghow <tankwanghow@gmail.com>
2014-03-12 17:38:16 +0800

    added permit params

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -3,6 +3,15 @@ module Helpers
     session[:user_id] = user ? (user.is_a?(User) ? user.id : create(:user).id) : nil
   end
 
+  def permitted_params params, keys
+    keys.map! { |t| t.to_s }
+    params.select do |k, v|
+      keys.include? k.to_s 
+    end.map do |k, v|
+      [k, [TrueClass, FalseClass].include?(v.class) ? v : v.to_s ]
+    end.to_h
+  end
+
   def add_dynamic_get_route route_hash
     begin
       _routes = Rails.application.routes
