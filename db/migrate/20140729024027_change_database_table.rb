f55a49ad2fd2c10d108b101a32eec9ebae066aed
tankwanghow <tankwanghow@gmail.com>
2014-07-29 17:52:15 +0800

    change numeric type to float, remove unwanted timestamp

---
+++ db/migrate/20140729024027_change_database_table.rb
@@ -0,0 +1,53 @@
+class ChangeDatabaseTable < ActiveRecord::Migration
+
+  def up
+    remove_column :premix_ingredients, :created_at
+    remove_column :premix_ingredients, :updated_at
+
+    change_table :premix_ingredients do |t|
+      t.change :actual_usage, :float
+      t.change :premix_usage, :float
+    end
+
+    change_table :formula_nutrients do |t|
+      t.change :max,    :float
+      t.change :min,    :float
+      t.change :actual, :float
+    end
+
+    change_table :formula_ingredients do |t|
+      t.change :max,    :float
+      t.change :min,    :float
+      t.change :actual, :float
+      t.change :weight, :float
+      t.change :shadow, :float
+    end
+
+    change_table :ingredient_compositions do |t|
+      t.change :value, :float
+    end
+
+    change_table :ingredients do |t|
+      t.change :package_weight, :float
+      t.change :cost, :float
+    end
+
+    change_table :formulas do |t|
+      t.change :batch_size, :float
+      t.change :target_bag_weight, :float
+      t.change :cost, :float
+    end
+
+    change_table :formulas do |t|
+      t.change :batch_size, :float
+      t.change :target_bag_weight, :float
+      t.change :cost, :float
+    end
+
+  end
+
+  def down
+    add_column :premix_ingredients, :created_at, :datetime
+    add_column :premix_ingredients, :updated_at, :datetime
+  end 
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,67 +11,65 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140714035721) do
+ActiveRecord::Schema.define(version: 20140729024027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "formula_ingredients", force: true do |t|
-    t.integer "formula_id",                                            null: false
-    t.integer "ingredient_id",                                         null: false
-    t.decimal "max",           precision: 12, scale: 6
-    t.decimal "min",           precision: 12, scale: 6
-    t.decimal "actual",        precision: 12, scale: 6, default: 0.0,  null: false
-    t.decimal "weight",        precision: 12, scale: 6
-    t.decimal "shadow",        precision: 12, scale: 6, default: 0.0,  null: false
-    t.boolean "use",                                    default: true, null: false
+    t.integer "formula_id",                   null: false
+    t.integer "ingredient_id",                null: false
+    t.float   "max"
+    t.float   "min"
+    t.float   "actual",        default: 0.0,  null: false
+    t.float   "weight"
+    t.float   "shadow",        default: 0.0,  null: false
+    t.boolean "use",           default: true, null: false
   end
 
   add_index "formula_ingredients", ["ingredient_id", "formula_id"], name: "index_formula_ingredients_on_ingredient_id_and_formula_id", unique: true, using: :btree
 
   create_table "formula_nutrients", force: true do |t|
-    t.integer "formula_id",                                          null: false
-    t.integer "nutrient_id",                                         null: false
-    t.decimal "max",         precision: 12, scale: 6
-    t.decimal "min",         precision: 12, scale: 6
-    t.decimal "actual",      precision: 12, scale: 6, default: 0.0,  null: false
-    t.boolean "use",                                  default: true, null: false
+    t.integer "formula_id",                 null: false
+    t.integer "nutrient_id",                null: false
+    t.float   "max"
+    t.float   "min"
+    t.float   "actual",      default: 0.0,  null: false
+    t.boolean "use",         default: true, null: false
   end
 
   add_index "formula_nutrients", ["nutrient_id", "formula_id"], name: "index_formula_nutrients_on_nutrient_id_and_formula_id", unique: true, using: :btree
 
   create_table "formulas", force: true do |t|
-    t.integer  "user_id",                                                  null: false
-    t.string   "name",                                                     null: false
-    t.decimal  "batch_size",        precision: 12, scale: 4, default: 0.0, null: false
-    t.decimal  "cost",              precision: 12, scale: 4, default: 0.0, null: false
+    t.integer  "user_id",                         null: false
+    t.string   "name",                            null: false
+    t.float    "batch_size",        default: 0.0, null: false
+    t.float    "cost",              default: 0.0, null: false
     t.text     "note"
-    t.integer  "lock_version",                               default: 0,   null: false
+    t.integer  "lock_version",      default: 0,   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "premix_bag_weight"
+    t.float    "target_bag_weight"
     t.integer  "bags_of_premix"
   end
 
-  add_index "formulas", ["user_id", "name"], name: "index_formulas_on_user_id_and_name", unique: true, using: :btree
-
   create_table "ingredient_compositions", force: true do |t|
-    t.integer "ingredient_id",                                        null: false
-    t.integer "nutrient_id",                                          null: false
-    t.decimal "value",         precision: 14, scale: 6, default: 0.0, null: false
+    t.integer "ingredient_id",               null: false
+    t.integer "nutrient_id",                 null: false
+    t.float   "value",         default: 0.0, null: false
   end
 
   add_index "ingredient_compositions", ["ingredient_id", "nutrient_id"], name: "index_ingredient_compositions_on_ingredient_id_and_nutrient_id", unique: true, using: :btree
 
   create_table "ingredients", force: true do |t|
-    t.integer  "user_id",                                                     null: false
-    t.string   "name",                                                        null: false
-    t.decimal  "package_weight", precision: 12, scale: 4, default: 0.1
-    t.decimal  "cost",           precision: 12, scale: 4, default: 0.0,       null: false
-    t.string   "status",                                  default: "using",   null: false
-    t.string   "category",                                default: "private", null: false
+    t.integer  "user_id",                            null: false
+    t.string   "name",                               null: false
+    t.float    "package_weight", default: 0.1
+    t.float    "cost",           default: 0.0,       null: false
+    t.string   "status",         default: "using",   null: false
+    t.string   "category",       default: "private", null: false
     t.text     "note"
-    t.integer  "lock_version",                            default: 0,         null: false
+    t.integer  "lock_version",   default: 0,         null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -92,14 +90,10 @@ ActiveRecord::Schema.define(version: 20140714035721) do
   add_index "nutrients", ["user_id", "name"], name: "index_nutrients_on_user_id_and_name", unique: true, using: :btree
 
   create_table "premix_ingredients", force: true do |t|
-    t.integer  "formula_id",                              null: false
-    t.integer  "ingredient_id",                           null: false
-    t.decimal  "package_weight", precision: 12, scale: 6
-    t.decimal  "actual_usage",   precision: 12, scale: 6
-    t.integer  "premix_usage"
-    t.decimal  "premix_perc",    precision: 6,  scale: 4
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer "formula_id",    null: false
+    t.integer "ingredient_id", null: false
+    t.float   "actual_usage"
+    t.float   "premix_usage"
   end
 
   add_index "premix_ingredients", ["ingredient_id", "formula_id"], name: "index_premix_ingredients_on_ingredient_id_and_formula_id", unique: true, using: :btree
