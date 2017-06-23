1bab6069fb0c5b3e6de2b0cb9ec6eb1cfc233e13
Tan Kwang How <tankwanghow@gmail.com>
2017-06-23 16:01:44 +0800

    follow cap-mb request

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@ role :db,  %w{deployer@106.186.124.38}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '106.186.124.38', user: 'deployer', roles: %w{web app}, my_property: :my_value
+server '106.186.124.38', user: 'deployer', roles: %w(app backup cron db web), my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
