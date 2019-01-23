821b01a1bf4088c9526e13ae1263d0bd4bc48266
Tan Kwang How <tankwanghow@gmail.com>
2019-01-23 16:27:54 +0800

    added :id to the query

--- app/controllers/ingredient_usages_controller.rb
+++ app/controllers/ingredient_usages_controller.rb
@@ -1,12 +1,12 @@
 class IngredientUsagesController < ApplicationController
 
   def index
-    sql = "select i.name, sum(fi.actual * f.usage_per_day) as qty, i.cost" +
+    sql = "select i.id, i.name, sum(fi.actual * f.usage_per_day) as qty, i.cost" +
           "  from formulas f inner join formula_ingredients fi on f.id = fi.formula_id" +
           " inner join ingredients i on i.id = fi.ingredient_id" +
           " where f.usage_per_day > 0" +
           "   and fi.actual > 0" +
-          " group by i.name, i.cost" +
+          " group by i.id, i.name, i.cost" +
           " order by sum(fi.actual * f.usage_per_day) * i.cost desc"
     @usages = ActiveRecord::Base.connection.execute(sql)
     @weight = @usages.inject(0) { |sum, hash| sum + hash["qty"].to_f }
